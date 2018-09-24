2fe247290afd3905638c08b2fb807e5939171d88
Reapeah <wildstyle3@yahoo.com>
2018-09-24 23:31:32 +0200

    This is a new commit for what I originally planned to be amended

---
+++ bot.py
@@ -0,0 +1,26 @@
+import tweepy
+from datetime import date
+import calendar
+import time
+
+my_date = date.today()
+calendar.day_name[my_date.weekday()]
+
+
+
+ckey ='aDnftecvhJVUmMcOkNEtWUhPu'
+csecret = 's7dSrZ75TfkTb1JIcelQeSb3vGETWqXdWvjm17yzZWuoR77l5X'
+akey ='369656146-WKyNfoPVEE0w1VTlu5VFQnFzUaNa880HkFfbk57D'
+asecret = 'kng4JBJLATTVGkUpWZ8KG6YJicLUNwpbxNrW09nBx7iLW'
+
+auth =  tweepy.OAuthHandler(ckey,csecret)
+auth.set_access_token(akey,asecret)
+
+api = tweepy.API(auth)
+c=1
+while True:
+    word='Hello' + str(c)
+    api.update_status(word )
+    c+=1
+    time.sleep(60)
+#api.update_status('Hello World')
