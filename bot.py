768b4d1616ce677c602128b0924b99596458903b
Reapeah <40251850+Reapeah@users.noreply.github.com>
2018-09-29 02:01:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ auth.set_access_token(akey,asecret)
 api = tweepy.API(auth)
 print(now.hour,now.minute)
 while True:
-    if now.hour == 0 and now.minute == 0:        
+    if now.hour == 0 and now.minute == 3:        
         word="Today is " + str(calendar.day_name[my_date.weekday()]) +" which means it's " + str(calendar.day_name[my_date.weekday()])       
         api.update_status(word)    
         time.sleep(120)
