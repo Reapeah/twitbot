b855d9909870c22726acbae9ef95159126a0bc8e
Reapeah <40251850+Reapeah@users.noreply.github.com>
2018-10-19 00:20:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,13 @@ import time
 import datetime
 
 
+cnow = datetime.datetime.now()
+
+my_date = date.today()
+calendar.day_name[my_date.weekday()]
+
+
+
 ckey = os.getenv('CKEY')
 csecret = os.getenv('CSECRET')
 akey = os.getenv('AKEY')
@@ -15,6 +22,11 @@ auth =  tweepy.OAuthHandler(ckey,csecret)
 auth.set_access_token(akey,asecret)
 
 api = tweepy.API(auth)
+print(now.hour,now.minute)
+
+curr_hour = 0
+curr_min = 50
+
 
 
 api.update_status('Hello World')
