aeeb5a97e1ff6da2505c0101e67237020cc07430
Reapeah <40251850+Reapeah@users.noreply.github.com>
2018-10-19 00:19:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,4 +6,15 @@ import time
 import datetime
 
 
+ckey = os.getenv('CKEY')
+csecret = os.getenv('CSECRET')
+akey = os.getenv('AKEY')
+asecret = os.getenv('ASECRET')
+
+auth =  tweepy.OAuthHandler(ckey,csecret)
+auth.set_access_token(akey,asecret)
+
+api = tweepy.API(auth)
+
+
 api.update_status('Hello World')
