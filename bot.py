2b9747e09ba608e6fb6883b620ed64965a8b1b74
Reapeah <40251850+Reapeah@users.noreply.github.com>
2018-10-19 00:19:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,33 +5,5 @@ import calendar
 import time
 import datetime
 
-now = datetime.datetime.now()
 
-my_date = date.today()
-calendar.day_name[my_date.weekday()]
-
-
-
-ckey = os.getenv('CKEY')
-csecret = os.getenv('CSECRET')
-akey = os.getenv('AKEY')
-asecret = os.getenv('ASECRET')
-
-auth =  tweepy.OAuthHandler(ckey,csecret)
-auth.set_access_token(akey,asecret)
-
-api = tweepy.API(auth)
-print(now.hour,now.minute)
-
-curr_hour = 0
-curr_min = 50
-
-while True:
-    now = datetime.datetime.now()
-    my_date = date.today()
-    calendar.day_name[my_date.weekday()]
-    if now.hour == 0 and now.minute == 0:        
-        word="Today is " + str(calendar.day_name[my_date.weekday()]) +" which means it's " + str(calendar.day_name[my_date.weekday()]) + ' #' + str(calendar.day_name[my_date.weekday()])            
-        api.update_status(word)    
-        time.sleep(120)
-#api.update_status('Hello World')
+api.update_status('Hello World')
